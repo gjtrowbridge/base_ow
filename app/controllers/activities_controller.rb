91fc3bdb32b3ebea15ce97c7f226b1bdcd5b3284
Greg Trowbridge <gjtrowbridge@gmail.com>
2012-02-12 18:11:12 -0800

    Allowed delete of locations within activity model, fixed deprecation w devise, added locations to activity show, added location show partial, fixed locations form

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -1,7 +1,3 @@
 class ActivitiesController < InheritedResources::Base
 
-	def new
-		@activity = Activity.new
-		1.times { @activity.locations.build }
-	end
 end
--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,5 +1,5 @@
 class Activity < ActiveRecord::Base
 	has_many :locations, :dependent => :delete_all
 	
-	accepts_nested_attributes_for :locations
+	accepts_nested_attributes_for :locations, :reject_if => lambda { |a| a[:address].blank? }, :allow_destroy => true
 end
